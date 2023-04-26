e934106c64fd1cf2e915c8df4ed8fd8857540eae
shenweiyan <shumlab@foxmail.com>
2023-04-26 17:04:45 +0800

    Update ExportDocMD2Hugo.py

--- ExportDocMD2Hugo.py
+++ ExportDocMD2Hugo.py
@@ -56,7 +56,7 @@ def __main__():
             tags_list.append(t)
     tags = '[\"%s\"]' % ('\",\"'.join(tags_list))
 
-    post_meata = "---\ntitle: '%s'\nslug: '%s'\ndate: '%s'\nlastmod: '%s'\ntype: post\npublished: true\nauthor: 沈维燕\ntags: %s\n---\n\n" % (title, slug, create, update, tags)
+    post_meata = "---\ntitle: '%s'\nslug: '%s'\ndate: '%s'\nlastmod: '%s'\ntype: posts\npublished: true\nauthor: 沈维燕\ntags: %s\n---\n\n" % (title, slug, create, update, tags)
     post_meata = post_meata + "> 📢本 文章同步自作者的[语雀知识库](https://www.yuque.com/shenweiyan/)，请点击[这里](%s)阅读原文。\n\n" % url
 
     md_url = url.strip("/")+'/markdown?plain=true&linebreak=false&anchor=false'
